096d81cf9c3031d2e2dc636d87dbb339058e83d6
Raktim <reachraktim@gmail.com>
2023-06-19 14:10:38 +0200

    Initial Commit

---
+++ GStreamerSwiftUIDemo/GStreamerSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GStreamerSwiftUIDemoApp.swift
+//  GStreamerSwiftUIDemo
+//
+//  Created by Raktim Bora on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct GStreamerSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
